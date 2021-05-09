0b7498d87c6b2751a5def125651411f8135869ca
Vicki <s1071930@gm.pu.edu.tw>
2021-05-09 21:19:45 +0800

    Multiple Devices Test2  (210509)

--- app/src/main/java/com/example/life/MainActivity.java
+++ app/src/main/java/com/example/life/MainActivity.java
@@ -7,12 +7,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.life.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -44,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         setMain();
 
