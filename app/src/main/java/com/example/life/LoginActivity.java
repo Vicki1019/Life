df5f4809d5b79d2c6ef95db0bff0abeff583f145
Vicki <s1071930@gm.pu.edu.tw>
2021-05-10 11:16:15 +0800

    Login design (210510)

--- app/src/main/java/com/example/life/LoginActivity.java
+++ app/src/main/java/com/example/life/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.life;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageButton login = (ImageButton) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton register = (ImageButton) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
