2598894caefa85712e0bc994a092b2065196f914
Vicki <s1071930@gm.pu.edu.tw>
2021-05-10 11:45:08 +0800

    Register design (210510)

--- app/src/main/java/com/example/life/RegisterActivity.java
+++ app/src/main/java/com/example/life/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.example.life;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button back_login = (Button) findViewById(R.id.back_login);
+        back_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton registerok = (ImageButton) findViewById(R.id.registerok);
+        registerok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
