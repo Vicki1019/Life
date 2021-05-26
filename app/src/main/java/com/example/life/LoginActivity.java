a07fc83baeb8ee1fb61d8e73d9779f02bb250873
Vicki <s1071930@gm.pu.edu.tw>
2021-05-26 10:13:21 +0800

    Mysql Connect (210526)

--- app/src/main/java/com/example/life/LoginActivity.java
+++ app/src/main/java/com/example/life/LoginActivity.java
@@ -1,19 +1,42 @@
 package com.example.life;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView result;
+    RequestQueue requesrg;
+    EditText account;
+    EditText passwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        result = findViewById(R.id.ckhint);
+        requesrg = Volley.newRequestQueue(this);
+        account = (EditText) findViewById(R.id.account);
+        passwd = (EditText) findViewById(R.id.passwd);
 
         ImageView login = (ImageView) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +57,32 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //String url = "http://10.0.2.2/life/login.php";
+        String url = "http://172.16.1.47/life/login.php";
+        StringRequest arrRequest =new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        result.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                result.setText(error.toString());
+            }
+        }
+        ) {
+            @Nullable
+            @org.jetbrains.annotations.Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email","account");
+                params.put("passwd","passwd");
+                return params;
+            }
+        };
+        requesrg.add(arrRequest);
     }
 }
