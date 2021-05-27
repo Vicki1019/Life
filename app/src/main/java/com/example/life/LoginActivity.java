2c6f36c13926a0fd83e0f944c7088e8258d1f143
Vicki <s1071930@gm.pu.edu.tw>
2021-05-27 09:27:04 +0800

    Login Test (210527)

--- app/src/main/java/com/example/life/LoginActivity.java
+++ app/src/main/java/com/example/life/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.life;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,14 +19,16 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView result;
+    TextView ckhint;
     RequestQueue requesrg;
-    EditText account;
+    EditText email;
     EditText passwd;
 
     @Override
@@ -33,21 +36,52 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        result = findViewById(R.id.ckhint);
+        ckhint = findViewById(R.id.ckhint);
         requesrg = Volley.newRequestQueue(this);
-        account = (EditText) findViewById(R.id.account);
+        email = (EditText) findViewById(R.id.account);
         passwd = (EditText) findViewById(R.id.passwd);
+        //String url = "http://10.0.2.2/life/login.php";
+        String url = "http://172.16.1.47/life/login.php"; //API URL(login.php)
 
+      //登入
         ImageView login = (ImageView) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+              //API串接MySQL(POST)
+                StringRequest arrRequest =new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                ckhint.setText(response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        ckhint.setText(error.toString());
+                    }
+                }
+                ) {
+                    @Nullable
+                    @org.jetbrains.annotations.Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("email",email.getText().toString());
+                        params.put("passwd",passwd.getText().toString());
+                        return params;
+                    }
+                };
+                requesrg.add(arrRequest);
+                if(ckhint.getText() == "\"登入成功\""){
+                    intent.setClass(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
+      //註冊
         ImageView register = (ImageView) findViewById(R.id.register);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,31 +92,5 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        //String url = "http://10.0.2.2/life/login.php";
-        String url = "http://172.16.1.47/life/login.php";
-        StringRequest arrRequest =new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        result.setText(response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                result.setText(error.toString());
-            }
-        }
-        ) {
-            @Nullable
-            @org.jetbrains.annotations.Nullable
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email","account");
-                params.put("passwd","passwd");
-                return params;
-            }
-        };
-        requesrg.add(arrRequest);
     }
 }
