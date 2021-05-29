684752ead5edce5e4672b3bdf1fb5de28812fc70
Vicki <s1071930@gm.pu.edu.tw>
2021-05-29 22:06:37 +0800

    Login --> OK Register --> Test V1.0(210529)

--- app/src/main/java/com/example/life/LoginActivity.java
+++ app/src/main/java/com/example/life/LoginActivity.java
@@ -1,15 +1,14 @@
 package com.example.life;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -19,67 +18,26 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView ckhint;
-    RequestQueue requesrg;
-    EditText email;
-    EditText passwd;
+    private EditText email, passwd;
+    private String lemail, lpasswd;
+    private ProgressBar loading;
+    //private static String url = "http://172.16.1.47/life/login.php"; //API URL(login.php)_彤家WIFI_IP
+    private String url = "http://192.168.210.110/life/login.php"; //API URL(login.php)_彤手機WIFI_IP
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ckhint = findViewById(R.id.ckhint);
-        requesrg = Volley.newRequestQueue(this);
-        email = (EditText) findViewById(R.id.account);
-        passwd = (EditText) findViewById(R.id.passwd);
-        //String url = "http://10.0.2.2/life/login.php";
-        String url = "http://172.16.1.47/life/login.php"; //API URL(login.php)
-
-      //登入
-        ImageView login = (ImageView) findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-              //API串接MySQL(POST)
-                StringRequest arrRequest =new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                ckhint.setText(response.toString());
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        ckhint.setText(error.toString());
-                    }
-                }
-                ) {
-                    @Nullable
-                    @org.jetbrains.annotations.Nullable
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put("email",email.getText().toString());
-                        params.put("passwd",passwd.getText().toString());
-                        return params;
-                    }
-                };
-                requesrg.add(arrRequest);
-                if(ckhint.getText() == "\"登入成功\""){
-                    intent.setClass(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
+        loading = findViewById(R.id.loading);
+        email = findViewById(R.id.account);
+        passwd = findViewById(R.id.passwd);
 
       //註冊
         ImageView register = (ImageView) findViewById(R.id.register);
@@ -91,6 +49,58 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+     public void login(View view){
+        lemail = email.getText().toString().trim();
+        lpasswd = passwd.getText().toString().trim();
+        loading.setVisibility(View.VISIBLE);
+
+      //Volley_POST
+        if(!lemail.equals("") && !lpasswd.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        loading.setVisibility(View.GONE);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("failure")) {
+                        loading.setVisibility(View.GONE);
+                        email.setError("帳號或密碼有誤");
+                        passwd.setError("帳號或密碼有誤");
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    loading.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("email", lemail);
+                data.put("passwd", lpasswd);
+                return data;
+            }
+          };
+          RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+          requestQueue.add(stringRequest);
+        }else{
+            loading.setVisibility(View.GONE);
+            if(lemail.equals("") && lpasswd.equals("")) {
+                email.setError("請輸入E-mail");
+                passwd.setError("請輸入密碼");
+            }else if(lemail.equals("")) {
+                email.setError("請輸入E-mail");
+                passwd.setError(null, null);
+            }else if(lpasswd.equals("")){
+                email.setError(null, null);
+                passwd.setError("請輸入密碼");
+            }
+        }
     }
 }
--- app/src/main/java/com/example/life/RegisterActivity.java
+++ app/src/main/java/com/example/life/RegisterActivity.java
@@ -6,10 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText nickname, email, passwd, passwdck;
+    private  String rnickname, remail, rpasswd, rpasswdck;
+    private ProgressBar loading;
+    //private static String url = "http://172.16.1.47/life/register.php"; //API URL(register.php)_彤家WIFI_IP
+    private String url = "http://192.168.210.110/life/register.php"; //API URL(register.php)_彤手機WIFI_IP
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +44,55 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-        ImageView registerok = (ImageView) findViewById(R.id.registerok);
-        registerok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        nickname = findViewById(R.id.nickname);
+        email = findViewById(R.id.myemail);
+        passwd = findViewById(R.id.password);
+        passwdck = findViewById(R.id.passwordck);
+        loading = findViewById(R.id.rloading);
+
+    }
+
+    public void register(View view) {
+        rnickname = nickname.getText().toString().trim();
+        remail = email.getText().toString().trim();
+        rpasswd = passwd.getText().toString().trim();
+        rpasswdck = passwdck.getText().toString().trim();
+        loading.setVisibility(View.VISIBLE);
+        if(!rpasswd.equals(rpasswdck)){
+            passwdck.setError("輸入密碼不一致");
+        }else if(!rnickname.equals("") && !remail.equals("") && !rpasswd.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        loading.setVisibility(View.GONE);
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                        Toast.makeText(RegisterActivity.this,"註冊成功，請重新登入", Toast.LENGTH_SHORT).show();
+                    } else if (response.equals("failure")) {
+                        loading.setVisibility(View.GONE);
+                        Toast.makeText(RegisterActivity.this,"此信箱已註冊過", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    loading.setVisibility(View.GONE);
+                    Toast.makeText(RegisterActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("name", rnickname);
+                    data.put("email", remail);
+                    data.put("passwd", rpasswd);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
     }
 }
