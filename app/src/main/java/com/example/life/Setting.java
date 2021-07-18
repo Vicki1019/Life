8afb875555e80eee581100aef261d02197573be3
Vicki <s1071930@gm.pu.edu.tw>
2021-07-18 17:49:13 +0800

    Fragmaent_Setting --> Activity_Usert (2021/07/18)

--- app/src/main/java/com/example/life/Setting.java
+++ app/src/main/java/com/example/life/Setting.java
@@ -1,12 +1,15 @@
 package com.example.life;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +26,6 @@ public class Setting extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     public Setting() {
         // Required empty public constructor
     }
@@ -59,6 +61,15 @@ public class Setting extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        ImageView userset = (ImageView) view.findViewById(R.id.setaccount);
+        userset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),Userset.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
