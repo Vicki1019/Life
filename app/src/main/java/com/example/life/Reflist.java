9466e465d6571e5a9292ce8c78354bc23671e620
ting20121108 <ting20121108@gmail.com>
2021-07-23 19:18:54 +0800

    before merge

--- app/src/main/java/com/example/life/Reflist.java
+++ app/src/main/java/com/example/life/Reflist.java
@@ -1,12 +1,17 @@
 package com.example.life;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 /**
@@ -55,7 +60,6 @@ public class Reflist extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
