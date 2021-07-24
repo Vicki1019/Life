6b098e4980cf87904d1aaa7a9f61d0e8050f20ab
410714558 <s1071455@gm.pu.edu.tw>
2021-07-24 17:51:47 +0800

    revise IP

--- app/src/main/java/com/example/life/LoginActivity.java
+++ app/src/main/java/com/example/life/LoginActivity.java
@@ -5,26 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, passwd;
+    private String lemail, lpasswd;
+    private ProgressBar loading;
+    private static String url = "http://192.168.82.110/PHP_API/life/login.php"; //API URL(login.php)
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ImageView login = (ImageView) findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        loading = findViewById(R.id.loading);
+        email = findViewById(R.id.account);
+        passwd = findViewById(R.id.passwd);
 
+      //註冊
         ImageView register = (ImageView) findViewById(R.id.register);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +49,58 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+     public void login(View view){
+        lemail = email.getText().toString().trim();
+        lpasswd = passwd.getText().toString().trim();
+        loading.setVisibility(View.VISIBLE);
+
+      //Volley_POST
+        if(!lemail.equals("") && !lpasswd.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        loading.setVisibility(View.GONE);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("failure")) {
+                        loading.setVisibility(View.GONE);
+                        email.setError("帳號或密碼有誤");
+                        passwd.setError("帳號或密碼有誤");
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    loading.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("email", lemail);
+                data.put("passwd", lpasswd);
+                return data;
+            }
+          };
+          RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+          requestQueue.add(stringRequest);
+        }else{
+            loading.setVisibility(View.GONE);
+            if(lemail.equals("") && lpasswd.equals("")) {
+                email.setError("請輸入E-mail");
+                passwd.setError("請輸入密碼");
+            }else if(lemail.equals("")) {
+                email.setError("請輸入E-mail");
+                passwd.setError(null, null);
+            }else if(lpasswd.equals("")){
+                email.setError(null, null);
+                passwd.setError("請輸入密碼");
+            }
+        }
     }
 }
