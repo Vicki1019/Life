f8cbe864785233b99fc81848c115b2b6bc1e59a3
Vicki <s1071930@gm.pu.edu.tw>
2021-07-26 19:19:10 +0800

    [更新]Unit spinner

--- app/src/main/java/com/example/life/MainActivity.java
+++ app/src/main/java/com/example/life/MainActivity.java
@@ -6,16 +6,21 @@ import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.DisplayMetrics;
+import android.util.JsonReader;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -29,12 +34,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private int i=1;
-    private static String url = "http://192.168.195.110/PHP_API/life/getunit.php"; //API URL(getunit.php)
+    private static String uniturl = "http://192.168.225.110/PHP_API/life/getunit.php"; //API URL(getunit.php)
     private static Spinner unit_spinner;
+    ArrayList<String> untiarytags = new ArrayList<>();
+    Boolean firstTime = true;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -69,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         setMain();
         BottomNavigationView navigation = findViewById(R.id.nav_view);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
     private void setMain() {  //主畫面
@@ -86,15 +100,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Refadd(View view) {
-        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);//創建AlertDialog.Builder
-        View refview = getLayoutInflater().inflate(R.layout.activity_refadd,null);//嵌入View
-        ImageView backDialog = refview.findViewById(R.id.refadd_back);//連結關閉視窗的Button
-        ImageView increase_btn = refview.findViewById(R.id.increase_btn);//增加數量的Button
-        ImageView decrease_btn = refview.findViewById(R.id.decrease_btn);//減少數量的Button
-        TextView quantity = refview.findViewById(R.id.refadd_quantity_text);//數量顯示
-        ImageView calendar_btn = refview.findViewById(R.id.calendar_btn);//選擇日期的Button
-        EditText date_input = refview.findViewById(R.id.refadd_data_input);//顯示日期
-        mBuilder.setView(refview);//設置View
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this); //創建AlertDialog.Builder
+        View refview = getLayoutInflater().inflate(R.layout.activity_refadd,null); //嵌入View
+        ImageView backDialog = refview.findViewById(R.id.refadd_back); //連結關閉視窗的Button
+        ImageView increase_btn = refview.findViewById(R.id.increase_btn); //增加數量的Button
+        ImageView decrease_btn = refview.findViewById(R.id.decrease_btn); //減少數量的Button
+        TextView quantity = refview.findViewById(R.id.refadd_quantity_text); //數量顯示
+        ImageView calendar_btn = refview.findViewById(R.id.calendar_btn); //選擇日期的Button
+        EditText date_input = refview.findViewById(R.id.refadd_data_input); //顯示日期
+        mBuilder.setView(refview); //設置View
         AlertDialog dialog = mBuilder.create();
         //關閉視窗的監聽事件
         backDialog.setOnClickListener(v1 -> {dialog.dismiss();});
@@ -119,20 +133,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //單位下拉選單
-        unit_spinner = (Spinner) findViewById(R.id.unit_spinner);
-        RequestQueue requesrq= Volley.newRequestQueue(this);
-        StringRequest strRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-            requesrq.add(strRequest);
+        Getunit();
 
         //日期選擇
         calendar_btn .setOnClickListener(new View.OnClickListener() {
@@ -172,4 +173,37 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setLayout(dm.widthPixels-230, ViewGroup.LayoutParams.WRAP_CONTENT);//設置螢幕寬度值
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//將原生AlertDialog的背景設為透明
     }
+
+    public void Getunit(){
+        unit_spinner = (Spinner) findViewById(R.id.unit_spinner);
+        RequestQueue requesrg = Volley.newRequestQueue(this);
+        StringRequest unitRequest = new StringRequest(Request.Method.GET, uniturl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    //將JSON字串放到JSONArray中
+                    JSONArray unitarr = new JSONArray(response);
+
+                    //解出JSON的資料寫入陣列中
+                    for (int i = 0; i < unitarr.length(); i++) {
+                        JSONObject jsonObject = unitarr.getJSONObject(i);
+                        String unittagname = jsonObject.getString("unit_cn");
+                        untiarytags.add(unittagname);
+                    }
+                }catch(JSONException e) {
+                    e.printStackTrace();
+                }
+                //將陣列跟Spinner物件連結
+                ArrayAdapter adapterunit = new  ArrayAdapter(MainActivity.this, android.R.layout.unit_spinner_item, untiarytags);
+                unit_spinner.setAdapter(adapterunit);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requesrg.add(unitRequest);
+    }
+
 }
