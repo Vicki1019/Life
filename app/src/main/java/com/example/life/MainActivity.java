c75d1719bd71d5ddfe1d2eb730360a6b83dcae58
Vicki <s1071930@gm.pu.edu.tw>
2021-08-03 01:01:52 +0800

    Unit spinner API連接成功 (20210803)

--- app/src/main/java/com/example/life/MainActivity.java
+++ app/src/main/java/com/example/life/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.life;
 
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.ContactsContract;
 import android.util.DisplayMetrics;
 import android.util.JsonReader;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -26,27 +30,32 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private int i=1;
-    private static String uniturl = "http://192.168.225.110/PHP_API/life/getunit.php"; //API URL(getunit.php)
-    private static Spinner unit_spinner;
-    ArrayList<String> untiarytags = new ArrayList<>();
-    Boolean firstTime = true;
+    private static String uniturl = "http://192.168.133.110/PHP_API/life/getunit.php"; //API URL(getunit.php)
+    ArrayList<String> unitlist = new ArrayList<>();
+    ArrayAdapter<String> unitAdapter;
+    RequestQueue requestQueue;
+
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -86,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setMain() {  //主畫面
-
         this.getSupportFragmentManager().beginTransaction().add(R.id.nav_host_fragment_activity_main,new Reflist()).commit();
     }
 
@@ -106,10 +114,12 @@ public class MainActivity extends AppCompatActivity {
         ImageView increase_btn = refview.findViewById(R.id.increase_btn); //增加數量的Button
         ImageView decrease_btn = refview.findViewById(R.id.decrease_btn); //減少數量的Button
         TextView quantity = refview.findViewById(R.id.refadd_quantity_text); //數量顯示
+        Spinner unitsp = (Spinner) refview.findViewById(R.id.unit_spinner); //單位下拉選單
         ImageView calendar_btn = refview.findViewById(R.id.calendar_btn); //選擇日期的Button
         EditText date_input = refview.findViewById(R.id.refadd_data_input); //顯示日期
         mBuilder.setView(refview); //設置View
         AlertDialog dialog = mBuilder.create();
+
         //關閉視窗的監聽事件
         backDialog.setOnClickListener(v1 -> {dialog.dismiss();});
         //增減數量的監聽事件
@@ -132,8 +142,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //單位下拉選單
-        Getunit();
+        //unit下拉選單
+        requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, uniturl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("unit");
+                    for(int j=0;j<jsonArray.length();j++){
+                        JSONObject jsonObject= jsonArray.getJSONObject(j);
+                        String unit_cn = jsonObject.optString("unit_cn");
+                        unitlist.add(unit_cn);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                unitAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, unitlist);
+                unitAdapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+                unitsp.setAdapter(unitAdapter);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
 
         //日期選擇
         calendar_btn .setOnClickListener(new View.OnClickListener() {
@@ -152,13 +187,13 @@ public class MainActivity extends AppCompatActivity {
                 }, year, month, date).show();
             }
         });
-
         dialog.show();
         DisplayMetrics dm = new DisplayMetrics();//取得螢幕解析度
         getWindowManager().getDefaultDisplay().getMetrics(dm);//取得螢幕寬度值
         dialog.getWindow().setLayout(dm.widthPixels-230, ViewGroup.LayoutParams.WRAP_CONTENT);//設置螢幕寬度值
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//將原生AlertDialog的背景設為透明
     }
+
 
     public void Shopadd(View view) {
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);//創建AlertDialog.Builder
@@ -173,37 +208,4 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setLayout(dm.widthPixels-230, ViewGroup.LayoutParams.WRAP_CONTENT);//設置螢幕寬度值
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//將原生AlertDialog的背景設為透明
     }
-
-    public void Getunit(){
-        unit_spinner = (Spinner) findViewById(R.id.unit_spinner);
-        RequestQueue requesrg = Volley.newRequestQueue(this);
-        StringRequest unitRequest = new StringRequest(Request.Method.GET, uniturl, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try{
-                    //將JSON字串放到JSONArray中
-                    JSONArray unitarr = new JSONArray(response);
-
-                    //解出JSON的資料寫入陣列中
-                    for (int i = 0; i < unitarr.length(); i++) {
-                        JSONObject jsonObject = unitarr.getJSONObject(i);
-                        String unittagname = jsonObject.getString("unit_cn");
-                        untiarytags.add(unittagname);
-                    }
-                }catch(JSONException e) {
-                    e.printStackTrace();
-                }
-                //將陣列跟Spinner物件連結
-                ArrayAdapter adapterunit = new  ArrayAdapter(MainActivity.this, android.R.layout.unit_spinner_item, untiarytags);
-                unit_spinner.setAdapter(adapterunit);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        requesrg.add(unitRequest);
-    }
-
 }
--- app/src/main/java/com/example/life/unitData.java
+++
@@ -1,4 +0,0 @@
-package com.example.life;
-
-public class unitData {
-}
