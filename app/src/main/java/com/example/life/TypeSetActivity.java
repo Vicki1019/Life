35d01946f598fda749fecadc128c30feab86e01a
Vicki <s1071930@gm.pu.edu.tw>
2021-08-28 17:57:33 +0800

    Merge(20210828)

--- app/src/main/java/com/example/life/TypeSetActivity.java
+++ app/src/main/java/com/example/life/TypeSetActivity.java
@@ -50,7 +50,7 @@ public class TypeSetActivity extends AppCompatActivity {
     //Session
     SessionManager sessionManager;
     //Volley
-    private static String kindurl = "http://192.168.227.110/PHP_API/index.php/Refrigerator/getkind";
+    private static String kindurl = "http://192.168.64.110/PHP_API/index.php/Refrigerator/getkind";
     RequestQueue kindrequestQueue;
     //RecyclerView
     RecyclerView myRecyclerView;
