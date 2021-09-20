c125b99ac46bbbb8a4dee2c539550f6b12e01a8f
Vicki <s1071930@gm.pu.edu.tw>
2021-09-20 19:01:49 +0800

    [新增]修改冰箱清單介面建立(20210920)

---
+++ app/src/main/java/com/example/life/Refrigerator/EditReflistActivity.java
@@ -0,0 +1,15 @@
+package com.example.life.Refrigerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.life.R;
+
+public class EditReflistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_reflist);
+    }
+}
