c7d383b43b28612701a3f02728dce723d9c62e67
Vicki <s1071930@gm.pu.edu.tw>
2021-10-04 22:18:38 +0800

    Merge

--- app/src/main/java/com/example/life/Login/LoginActivity.java
+++ app/src/main/java/com/example/life/Login/LoginActivity.java
@@ -21,6 +21,13 @@ import com.android.volley.toolbox.Volley;
 import com.example.life.MainActivity;
 import com.example.life.R;
 import com.example.life.Manager.SessionManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 
 import org.json.JSONArray;
