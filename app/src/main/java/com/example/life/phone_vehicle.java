666738d60f95f0d63913462c8781dcd0f01aaf9b
Vicki <s1071930@gm.pu.edu.tw>
2021-10-26 21:58:51 +0800

    [更新]載具設定按鈕

--- app/src/main/java/com/example/life/phone_vehicle.java
+++ app/src/main/java/com/example/life/phone_vehicle.java
@@ -9,16 +9,37 @@ import android.view.View;
 import android.widget.Button;
 
 public class phone_vehicle extends AppCompatActivity {
-    Button btn;
+    //Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_vehicle);
-        btn = (Button) findViewById(R.id.button);
+        //跳轉到財政部_未申請
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.einvoice.nat.gov.tw/APCONSUMER/BTC501W/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        //跳轉到財政部_已申請
+        Button btn6 = (Button) findViewById(R.id.button6);
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.einvoice.nat.gov.tw/index?open=b3Blbg");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        /*btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(button2());
-        btn.setOnClickListener(button6());
+        btn.setOnClickListener(button6());*/
     }
-    //跳轉到財政部_未申請
+   /* //跳轉到財政部_未申請
     private View.OnClickListener button2() {
         Uri uri = Uri.parse("https://www.einvoice.nat.gov.tw/APCONSUMER/BTC501W/");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
@@ -30,5 +51,5 @@ public class phone_vehicle extends AppCompatActivity {
         Uri uri = Uri.parse("https://www.einvoice.nat.gov.tw/index?open=b3Blbg");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
-    }
+    }*/
 }
