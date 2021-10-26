ff02aae74b8489d4a99a163c9ecd5fc45cf08da4
Vicki <s1071930@gm.pu.edu.tw>
2021-10-26 21:16:07 +0800

    [更新]Line Notify介面更新為switch選擇

--- app/src/main/java/com/example/life/Setting/NotifySetActivity.java
+++ app/src/main/java/com/example/life/Setting/NotifySetActivity.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.android.volley.RequestQueue;
+import com.example.life.MainActivity;
 import com.example.life.R;
 
 public class NotifySetActivity extends AppCompatActivity {
@@ -23,13 +26,40 @@ public class NotifySetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify_set);
 
-        Uri notify_uri = Uri.parse("http://172.16.1.44/PHP_API/index.php/LineNotify/LineAuthorize");
-        Intent intent = new Intent(Intent.ACTION_VIEW, notify_uri);
-        startActivity(intent);
+        Button notify_back_btn = (Button) findViewById(R.id.notify_back_btn);
+        notify_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(NotifySetActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Line Notify設定
+        Switch notify_switch = (Switch) findViewById(R.id.notify_switch);
+        notify_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true){
+                    Uri notify_uri = Uri.parse("http://172.16.1.44/PHP_API/index.php/LineNotify/LineAuthorize");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, notify_uri);
+                    startActivity(intent);
+                }
+            }
+        });
 
         /*WebView webview = (WebView) findViewById(R.id.notify);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.setWebViewClient(new WebViewClient()); //不調用系統瀏覽器
         webview.loadUrl("http://172.16.1.44/PHP_API/index.php/LineNotify/LineAuthorize");*/
+    }
+
+    // Disable back button
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.setClass(NotifySetActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
