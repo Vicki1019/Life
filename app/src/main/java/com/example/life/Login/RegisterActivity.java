6d1eb5c638c4f01976d53cafe18897513d006345
Vicki <s1071930@gm.pu.edu.tw>
2021-11-19 17:32:33 +0800

    ip

--- app/src/main/java/com/example/life/Login/RegisterActivity.java
+++ app/src/main/java/com/example/life/Login/RegisterActivity.java
@@ -36,8 +36,8 @@ public class RegisterActivity extends AppCompatActivity {
     private  String rnickname, remail, rpasswd, rpasswdck, rphoto;
     private ProgressBar loading;
     private ImageView profile_photo_register;
-    192.168.170.110
-    private static String url = "http://192.168.39.110/PHP_API/index.php/Login/register";
+    private Bitmap bitmap;
+    private static String url = "http://192.168.170.110/PHP_API/index.php/Login/register";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
