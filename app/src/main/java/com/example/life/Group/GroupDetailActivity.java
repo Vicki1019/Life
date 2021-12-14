a763c738e18727f08e4236798418d56b999b740a
Vicki <s1071930@gm.pu.edu.tw>
2021-12-14 10:24:14 +0800

    ip

--- app/src/main/java/com/example/life/Group/GroupDetailActivity.java
+++ app/src/main/java/com/example/life/Group/GroupDetailActivity.java
@@ -37,7 +37,7 @@ public class GroupDetailActivity extends AppCompatActivity {
     ImageView group_back;
     TextView group_name_title, group_no_copy;
     //Volley
-    private static String groupurl = "http://172.16.1.35/PHP_API/index.php/Group/get_group_member";
+    private static String groupurl = "http://10.0.34.233/PHP_API/index.php/Group/get_group_member";
     RequestQueue grouprequestQueue;
     //RecyclerView
     RecyclerView groupRecyclerView;
